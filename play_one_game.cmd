274f40caf6a14efb115df1ca47a3ec65f2d3f795
jan709 <j.vandieren@students.uu.nl>
2014-01-20 15:27:41 +0100

    playing files

---
+++ play_one_game.cmd
@@ -0,0 +1,15 @@
+@echo off
+%~dp0..\Python\App\python.exe "%~dp0playgame.py" ^
+ --engine_seed 42 ^
+ --player_seed 42 ^
+ --end_wait=0.5 ^
+ --verbose ^
+ -E -e ^
+ --log_dir game_logs ^
+ --turntime=20000000 ^
+ --loadtime=1000000 ^
+ --turns 10 ^
+ --nolaunch ^
+ --map_file "%~dp0maps\practical2\corridor.map" ^
+ "%~dp0..\Practical2Bot\bin\Debug\MyBot.exe" ^
+ "%~dp0..\Python\App\python.exe ""%~dp0sample_bots\dommebot\MyBot.py3"""
