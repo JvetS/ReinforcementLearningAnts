f7172ebac8f9369d6bdca7c096e3c50a9a48ea7e
jan709 <j.vandieren@students.uu.nl>
2014-01-24 15:46:43 +0100

    play files up to dat voor inleveren

--- play_many_games.cmd
+++ play_many_games.cmd
@@ -1,16 +1,14 @@
 @echo off
-FOR /l %%e IN (1,1,200) DO (
 %~dp0..\Python\App\python.exe "%~dp0playgame.py" ^
  --engine_seed 42 ^
  --player_seed 42 ^
- --end_wait=0.5 ^
- --verbose ^
- -E -e ^
+ --end_wait=5 ^
+ --rounds 100 ^
  --log_dir game_logs ^
  --turntime=20000000 ^
  --loadtime=1000000 ^
- --turns 10 ^
+ --turns 1000 ^
  --nolaunch ^
- --map_file "%~dp0maps\practical2\corridor.map" ^
+ --map_file "%~dp0maps\practical2\cross15.map" ^
  "%~dp0..\Practical2Bot\bin\Debug\MyBot.exe" ^
- "%~dp0..\Python\App\python.exe ""%~dp0sample_bots\dommebot\MyBot.py3""" )
+ "%~dp0..\Python\App\python.exe ""%~dp0sample_bots\dommebot\MyBot.py3"""
--- play_one_game.cmd
+++ play_one_game.cmd
@@ -1,15 +1,14 @@
 @echo off
+
 %~dp0..\Python\App\python.exe "%~dp0playgame.py" ^
- --engine_seed 42 ^
- --player_seed 42 ^
- --end_wait=0.5 ^
+ --engine_seed 90 ^
+ --player_seed 90 ^
+ --end_wait=10 ^
  --verbose ^
- -E -e ^
  --log_dir game_logs ^
  --turntime=20000000 ^
  --loadtime=1000000 ^
- --turns 10 ^
- --nolaunch ^
- --map_file "%~dp0maps\practical2\corridor.map" ^
- "%~dp0..\Practical2Bot\bin\Debug\MyBot.exe" ^
+ --turns 1000 ^
+ --map_file "%~dp0maps\Practical2\cross15.map" ^
+ "%~dp0..\Practical2Bot\bin\Release\MyBot.exe" ^
  "%~dp0..\Python\App\python.exe ""%~dp0sample_bots\dommebot\MyBot.py3"""
